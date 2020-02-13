28ddd47111b690be4eec8b020e43cda9f4353816
Brent Dillingham <brentdillingham@gmail.com>
2020-02-13 15:44:19 -0500

    Add build-cache-restore.sh

---
+++ .heroku/build-cache-save.sh
@@ -0,0 +1,12 @@
+AWS_ACCESS_KEY=$(cat $ENV_DIR/AWS_ACCESS_KEY)
+AWS_SECRET_KEY=$(cat $ENV_DIR/AWS_SECRET_KEY)
+
+BUCKET=kajabi-pipeline-staging-build-cache
+
+cd $CACHE_DIR
+
+tar -czf vendor tmp public | rclone rcat :s3:$BUCKET/build-cache.tar.gz \
+  --s3-access-key-id $AWS_ACCESS_KEY \
+  --s3-secret-access-key $AWS_SECRET_KEY \
+  --s3-provider AWS \
+  --s3-region us-east-1
